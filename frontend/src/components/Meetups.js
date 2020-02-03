59999eb8c39c7bac67de82ffd2b27ca27509133b
truthblue82 <truthblue82@gmail.com>
2020-02-03 17:01:49 +0700

    rename client_src to frontend

---
+++ frontend/src/components/Meetups.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Meetups extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Meetups</h1>
+            </div>
+        )
+    }
+}
+
+export default Meetups;
